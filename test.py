b5bddb610606785b3355534d2cfdfa768af12ccc
Naimat Ullah <gul405018@gmail.com>
2026-06-12 12:29:43 +0500

    a

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
 import re
+from bs4 import BeautifulSoup
+
 with open('index.html', 'r', encoding='utf-8') as f:
     html = f.read()
-matches = re.findall(r'<div\s+id="[^"]+"\s+class="tab-view[^>]*>', html)
-for m in matches:
-    print(m)
+
+soup = BeautifulSoup(html, 'html.parser')
+
+def count_children(node_id):
+    node = soup.find(id=node_id)
+    if node:
+        print(f"Children of {node_id}: {[c.get('id') for c in node.find_all('div', recursive=False) if c.get('id')]}")
+
+count_children('app-shell')
+count_children('admin-view')
+count_children('manager-view')
+count_children('employee-view')
