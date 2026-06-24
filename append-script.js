641b3b368b29d9690a0d8b009fcd4e77e8595185
Naimat Ullah <gul405018@gmail.com>
2026-06-24 18:33:49 +0500

    chore: remove unused and scratch files

--- append-script.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-const target = "document.write('<script src=\"assets.js?v=' + cacheBuster + '\"><\\/script>');";
-const replacement = target + "\n        document.write('<script src=\"reports.js?v=' + cacheBuster + '\"><\\/script>');";
-html = html.replace(target, replacement);
-fs.writeFileSync('index.html', html, 'utf8');
-console.log("Done");
