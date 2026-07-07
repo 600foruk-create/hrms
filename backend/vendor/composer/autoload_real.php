55b41d658f933bd4b1209b779017e64abe8026b2
Naimat Ullah <gul405018@gmail.com>
2026-07-07 16:53:20 +0500

    Fix composer autoloader platform check

--- backend/vendor/composer/autoload_real.php
+++ backend/vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit7d34c8de3daf7e9a7b862d0ae1c49ee1
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit7d34c8de3daf7e9a7b862d0ae1c49ee1', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit7d34c8de3daf7e9a7b862d0ae1c49ee1', 'loadClassLoader'));
